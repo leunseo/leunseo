18c0583f593c8865335cead2ca9f486f7fa1007e
leunseo <94345492+leunseo@users.noreply.github.com>
2021-11-16 14:02:39 +0900

    Create Time.py

---
+++ Time.py
@@ -0,0 +1,10 @@
+class Time:
+    def __init__(self, hour, minute, second):
+    self.hour = hour
+    self.minute = minute
+    self.second = second
+    
+    def is_valid_time(self, ):
+      
+    
+    
