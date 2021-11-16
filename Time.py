22418a10d0fc87f7f47ee45e97d0948bdc27a1f1
leunseo <94345492+leunseo@users.noreply.github.com>
2021-11-16 19:33:45 +0900

    Update Time.py

--- Time.py
+++ Time.py
@@ -4,8 +4,14 @@ class Time:
         self.minute = minute
         self.second = second
     
-    def is_valid_time(self, ):
+    def is_valid_time(self,21,09,59):
         if {0}>24 or {1}>60 or {1}>60:
              print('{0}:{1}:{2}는 존재하지 않는 시간입니다.'.format(self.hour, self.minute, self.second))
-        elif:
+        else:
+             print('{0}:{1}:{2}는 존재하는 시간입니다.'.format(self.hour, self.minute, self.second))
+                
+    def is_valid_time(self,23,59,60):
+        if {0}>24 or {1}>60 or {1}>60:
+             print('{0}:{1}:{2}는 존재하지 않는 시간입니다.'.format(self.hour, self.minute, self.second))
+        else:
              print('{0}:{1}:{2}는 존재하는 시간입니다.'.format(self.hour, self.minute, self.second))
